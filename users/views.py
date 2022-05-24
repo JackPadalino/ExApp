f55ff57d0cd3772151c61ad6d9b7717f320bf233
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-24 19:39:45 -0400

    Updated 'register' view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
@@ -10,10 +11,12 @@ def login(request):
     return render(request,'users/login.html',context)
 
 def register(request):
-    if request.metho == "POST":
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
+            messages.success(request,f'Account created for {username}!')
+            return redirect('main-home')
     else:
         form = UserCreationForm()
     context = {
