e3c7df66e01104e34fcae308d0a113ebf423435c
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-24 20:59:26 -0400

    Updated 'template_name' in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     #path('login/',views.login,name='users-login'),
-    path('login/',auth_views.LoginView.as_view(),name='users-login'),
-    path('logou/',auth_views.LogoutView.as_view(),name='users-logout'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='users-login'),
+    path('logou/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='users-logout'),
     path('register/',views.register,name='users-register')
 ]
