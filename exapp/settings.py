7df3ce4f3339785561d646ead0544ccb5e3d2461
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-24 20:39:28 -0400

    Updated settings.py register form with crispy forms

--- exapp/settings.py
+++ exapp/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'main',
     'projects',
     'users',
-    'crispy_forms'
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,4 +127,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
