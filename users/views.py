cc0a19146e7c7b3ab215781d0ea20254336519f3
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-24 20:25:39 -0400

    Updated views.py to use custom form

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def login(request):
@@ -12,14 +13,14 @@ def login(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account created for {username}!')
             return redirect('users-login')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     context = {
         'title':'Register',
         'form':form
