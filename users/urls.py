92b7a80b5f9adc5d3b01210ae9ab4fddb814a58c
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-24 20:53:02 -0400

    Added class based views to url patterns

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/',views.login,name='users-login'),
+    #path('login/',views.login,name='users-login'),
+    path('login/',auth_views.LoginView.as_view(),name='users-login'),
+    path('logou/',auth_views.LogoutView.as_view(),name='users-logout'),
     path('register/',views.register,name='users-register')
 ]
