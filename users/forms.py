847866f965b4e156d3bddb479a6585a80f546b1e
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-26 08:01:44 -0400

    Added period field to user models and forms

--- users/forms.py
+++ users/forms.py
@@ -2,18 +2,28 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 #from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-#from .models import Profile
+from .models import Profile
 
 # here we are inheriting the user creating form that comes with Django, but we are adding the email field so 
 # we can validate a user using their email
 class UserRegisterForm(UserCreationForm):
+    periods = [
+        ('',''),
+        ('1','1'),
+        ('2','2'),
+        ('3','3'),
+        ('4','4'),
+        ('6','6'),
+        ('7','7'),
+    ]
     email = forms.EmailField()
+    period = forms.CharField()
     class Meta:
         model = User
-        fields = ['username','email','password1','password2']
+        fields = ['username','email','period','password1','password2']
 
-'''
 # this will create a form that will allow a user to update their profile information
+# a model form creates a form that is able to interact with a specific model
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
     
@@ -21,6 +31,7 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username','email']
 
+'''
 # this will create a form that will alow a user update their profile picture
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
--- users/models.py
+++ users/models.py
@@ -3,7 +3,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
+    periods = [
+        ('',''),
+        ('1','1'),
+        ('2','2'),
+        ('3','3'),
+        ('4','4'),
+        ('6','6'),
+        ('7','7'),
+    ]
     user = models.OneToOneField(User,on_delete=models.CASCADE)
+    period = models.CharField(max_length=1,choices=periods,default='')
+
     #project_pics = 
 
     def __str__(self):
