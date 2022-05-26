c3806273f4dd593c85f105415fa5aa00de36b695
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-25 21:31:43 -0400

    Moved routes from user/urls.py to exapp/urls.py

--- exapp/urls.py
+++ exapp/urls.py
@@ -20,8 +20,8 @@ from users import views as user_views
 
 urlpatterns = [
     path('',include('main.urls')),
-    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='users-login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='users-logout'),
+    path('users/login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='users-login'),
+    path('users/logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='users-logout'),
     path('register/',user_views.register,name='users-register'),
     path('projects/',include('projects.urls')),
     path('users/',include('users.urls')),
