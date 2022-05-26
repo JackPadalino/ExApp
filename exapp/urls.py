7edacf61e020ecb0ea9fbc0635f016df123cb85e
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-25 20:56:28 -0400

    Moved routes from user/urls.py to exapp/urls.py

--- exapp/urls.py
+++ exapp/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from users import views as user_views
 
 urlpatterns = [
     path('',include('main.urls')),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='users-login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='users-logout'),
+    path('register/',user_views.register,name='users-register'),
     path('projects/',include('projects.urls')),
     path('users/',include('users.urls')),
     path('admin/', admin.site.urls),
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from users import views as user_views
 
 urlpatterns = [
     #path('login/',views.login,name='users-login'),
-    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='users-login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='users-logout'),
-    path('register/',user_views.register,name='users-register'),
     path('profile/',user_views.profile,name='users-profile')
 ]
