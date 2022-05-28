b34227eefea9cb12e2c854ccce12767b3e7c939f
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-28 12:18:44 -0400

    Added notes

--- notes.py
+++ notes.py
@@ -1,3 +1,8 @@
-Features - 
-- Filter students by class period
-- 
+# - Features - #
+
+# Filter students by class period
+# Students can upload multiple images to their project page
+# Comments
+# Likes
+# Filter by top liked
+# 
