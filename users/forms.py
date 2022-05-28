faa678a66a151a0f1fd3648a200c761933c4cd2a
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-28 10:01:50 -0400

    Added extra fields to user and profile forms

--- users/forms.py
+++ users/forms.py
@@ -58,21 +58,29 @@ class UserProfileForm(forms.ModelForm):
         model = Profile
         fields = ['period']
 
-
 # this will create a form that will allow a user to update their profile information
 # a model form creates a form that is able to interact with a specific model
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
-    
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+
     class Meta:
         model = User
-        fields = ['username','email']
+        fields = ['first_name','last_name','username','email']
 
-
-'''
 # this will create a form that will alow a user update their profile picture
 class ProfileUpdateForm(forms.ModelForm):
+    periods = [
+        (1,1),
+        (2,2),
+        (3,3),
+        (4,4),
+        (6,6),
+        (7,7),
+    ]
+    period = forms.ChoiceField(choices=periods)
     class Meta:
         model = Profile
-        fields = ['image']
-'''
+        #fields = ['image']
+        fields = ['project_title','project_blurb','project_description','period']
