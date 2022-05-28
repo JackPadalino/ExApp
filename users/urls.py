d20d7b2e7ebf1b5ec0d9a245cbfb86fa84bb2440
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-28 17:15:43 -0400

    Added add-comment url

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from users import views as user_views
-from .views import ProjectDetailView
+from .views import ProjectDetailView,CommentCreateView
 
 urlpatterns = [
     #path('login/',views.login,name='users-login'),
     path('projects/',user_views.displayprojects,name='users-studentprojects'),
     path('profile/',user_views.profile,name='users-profile'),
     path('myproject/',user_views.myproject,name='users-myproject'),
-    path('projects/<int:user_pk>/',ProjectDetailView,name='project-details'),
+    path('projects/<int:user_id>/',ProjectDetailView,name='project-details'),
+    path('projects/<int:user_id>/comment/',CommentCreateView.as_view(),name='add-comment'),
 ]
