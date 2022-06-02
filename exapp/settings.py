75bb2764e957e3361420a21a8a6bb0e9a6ccbe0e
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-06-01 21:58:07 -0400

    Updated settings.py with environment variables

--- exapp/settings.py
+++ exapp/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gv1p-m7#v=vitf+88ce4jhso@7-(-dve10+@vwk*m=@4l3++do'
+SECRET_KEY = os.environ.get('EXAPP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['exap.herokuapp.com']
 
 
 # Application definition
