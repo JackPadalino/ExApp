7d198623c94b27eed802f0329073b860c9b26837
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-06-06 21:18:11 -0400

    Cleaned up forms.py

--- users/forms.py
+++ users/forms.py
@@ -12,7 +12,6 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name','last_name','username','email','password1','password2']
-        #fields = ['first_name','last_name','email','username','password1','password2']
 
 class UserProfileForm(forms.ModelForm):
     periods = [
@@ -24,22 +23,15 @@ class UserProfileForm(forms.ModelForm):
         (7,7),
     ]
     period = forms.ChoiceField(choices=periods)
-
     class Meta:
         model = Profile
-        #fields = ['period']
         fields = []
 
 # this will create a form that will allow a user to update their profile information
 # a model form creates a form that is able to interact with a specific model
 class UserUpdateForm(forms.ModelForm):
-    #email = forms.EmailField()
-    #first_name = forms.CharField(max_length=100)
-    #last_name = forms.CharField(max_length=100)
-
     class Meta:
         model = User
-        #fields = ['first_name','last_name','username','email']
         fields = ['username','email']
 
 # this will create a form that will alow a user update their profile picture
@@ -55,20 +47,9 @@ class ProfileUpdateForm(forms.ModelForm):
     period = forms.ChoiceField(choices=periods)
     class Meta:
         model = Profile
-        #fields = ['image']
         fields = []
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
-
-#class ProjectVideoForm(forms.ModelForm):
-#    class Meta:
-#        model = ProjectVideo
-#        fields = ['URL']
-
-class VideoForm(forms.ModelForm):
-    class Meta:
-        model = Project
-        fields = ['video']
